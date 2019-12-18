4d1c81bdba19a3d51f2bea5256e5c704874b5d3a
Leandro Custodio <leandro.arthurcustodio@gmail.com>
2019-12-18 09:39:40 -0300

    Foi Adicionada a Opção de Deletar e de Buscar Produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,6 @@
 class ProdutosController < ApplicationController
     def index
-        @produto = Produto.order( nome: :desc).limit 5
+        @produto = Produto.order( nome: :asc)
 
         @produtoBarato = Produto.order( :preco).limit 1
 
@@ -11,8 +11,18 @@ class ProdutosController < ApplicationController
         produto = params.require(:produto).permit(:nome,  :descricao, :preco, :quantidade)
         
         Produto.create produto 
-        redirect_to root_path
+        redirect_to root_url
     end
 
+    def destroy
+        id = params[:id]
+        Produto.destroy id
+        redirect_to root_url
+    end
+    
+    def busca
+        nome = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{nome}%"
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-    post "produtos", to:  "produtos#create"
-    get "produtos/new",to: "produtos#new" 
+    get "produtos/busca", to: "produtos#busca", as: :busca_produto
+    resources :produtos, only: [:new, :create, :destroy]
     root to: "produtos#index"
 end
+
+
+
+    #delete "produtos/:id/remover", to: "produtos#destroy", as: :produto
+   # post "produtos", to: "produtos#create"
+    #get "produtos/new", to: "produtos#new"
